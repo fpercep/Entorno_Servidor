f805f390207d05503a35a59f7137dc668a87327d
PC Clase <fpercep889@g.educaand.es>
2025-11-14 20:52:39 +0100

    Practica

---
+++ Proyecto_1/tema2/cedenas_caracteres/relacion_ejer/cadenas_r1_ejer1.php
@@ -0,0 +1,10 @@
+<?php
+$matriz = ["Perro", "Gato123", "gallina123", "Pérez", "Pepa"];
+$matriz_NP = [];
+
+foreach ($matriz as $key => $value) {
+    echo "$value <br/>";
+    if (strstr($value, "p") != " "){
+
+    }
+};
